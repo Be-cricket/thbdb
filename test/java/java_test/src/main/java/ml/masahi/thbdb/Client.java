bc18b8b8aab9049415956332232dc2a1fc784fd2
Masahiro YASAKA <gogs@fake.local>
2019-01-31 21:38:25 +0900

    Implement test code in java..

---
+++ test/java/java_test/src/main/java/ml/masahi/thbdb/Client.java
@@ -0,0 +1,37 @@
+package ml.masahi.thbdb;
+
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import ml.masahi.thbdb.gen_java.*;
+
+public class Client {
+    
+    public static void main(String[] args) throws Exception {
+	TTransport transport = new TSocket("localhost", 9090);
+	TProtocol protocol = new TBinaryProtocol(transport);
+	Basic.Client client = new Basic.Client(protocol);
+	transport.open();
+	String result = client.hello("Amedama");
+	System.out.println(result);
+
+	result = client.hello("あめだま");
+	System.out.println(result);
+
+
+	client.put( "a" , "a-value" );
+	
+	
+	result = client.get( "a" );
+	System.out.println(result);
+
+	client.put( "テスト", "テストーバリュー" );
+	
+
+	result = client.get( "テスト" );
+	System.out.println(result);
+
+	transport.close();
+    }
+}
